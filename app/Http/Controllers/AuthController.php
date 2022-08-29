7117dc01c7daf1f4ad16678e04191d2e174af909
Cat-Nile <hyukim@hyukui-Macmini.local>
2022-08-30 01:11:32 +0900

    Feature: 회원가입 시 폼 검증 추가

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,12 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+
+
 
 class AuthController extends Controller
 {
     public function signUp(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:4',
+            'password_confirmation' => 'required|same:password',
+        ]);
+
+        if($validator->fails()) {
+            return response()->json(['message' => '폼 검증 실패', 'errors' => $validator->errors()], 422);
+        }
+
         $params = $request->only(['name', 'email', 'password']);
         $params['password'] = bcrypt($params['password']); 
         $user = User::create($params);
